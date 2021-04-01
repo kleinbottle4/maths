3d9cc3169a0fe95cf94d6391865b6885b6d19106
kleinbottle4 <kleinbottle4@gmail.com>
2021-04-01 13:01:20 +0100

    colours

--- plotlines/main.py
+++ plotlines/main.py
@@ -1,32 +1,35 @@
 import pygame
 from pygame.locals import *
-from cmath import *
+import cmath
 
 WIDTH = 500
 HALFWIDTH = int(WIDTH/2)
-BG = 0xffffff
-FG = 0x000000
 
-GAP = 1
-RES = 0.01
+BG = 0x000000
+FG = (0xffffff, 0xffff00)
 
-FN = lambda z : z**3
+GAP = 10
+RES = 0.1
 
-MAX = 25
+FN = lambda z : z**2
+
+MAX = 250
 MIN = -MAX
 
-ZOOMX = 100
-ZOOMY = 100
+ZOOMX = 1/100
+ZOOMY = ZOOMX
 
 DISPLAY = pygame.display.set_mode((WIDTH, WIDTH))
 
-def main():
-    DISPLAY.fill(BG)
-    for i in range(MIN, MAX, GAP):
+def main(argv):
+    bg = get_bg(argv, BG)
+    fg = get_fg(argv, FG, bg)
+    DISPLAY.fill(bg)
+    for i in range(MIN, MAX+1, GAP):
         print(f"Im(z) = {i} ...")
-        proc_line(i,False)
+        proc_line(i, True, fg)
         print(f"Re(z) = {i} ...")
-        proc_line(i,True)
+        proc_line(i, False, fg)
         pygame.display.update()
         if is_quit(): return 0
     print('done')
@@ -39,8 +42,12 @@ def is_quit():
 def transform_line(line, fn):
     rtn = []
     for z in line:
-        w = fn(complex(z[0],z[1]))
-        rtn.append((w.real, w.imag))
+        try:
+            w = fn(complex(z[0],z[1]))
+        except:
+            continue
+        else:
+            rtn.append((w.real, w.imag))
     return rtn
 
 def to_pygame_coords(line):
@@ -51,15 +58,38 @@ def line(k, _min, _max, is_h, res):
     rtn = []
     i = _min
     while i <= _max:
-        rtn.append( (is_h * i + (not is_h) * k, (not is_h) * i + is_h
-            * k) )
+        rtn.append( (is_h * i + (not is_h) * k, (not is_h) * i + is_h * k) )
         i += res
     return rtn
 
-def proc_line(i, is_h):
-    pygame.draw.lines( DISPLAY, FG, False,
-            to_pygame_coords( transform_line(
-                    line( i, -HALFWIDTH, HALFWIDTH, is_h, RES), FN)))
+def proc_line(i, is_h, fg):
+    line1 = line( i, -HALFWIDTH, HALFWIDTH, is_h, RES)
+    line2 = transform_line(line1, FN)
+    line3 = to_pygame_coords(line2)
+    colour = fg[is_h]
+    try:
+        pygame.draw.lines( DISPLAY, colour, False, line3)
+    except:
+        print("failed")
 
-if __name__ == '__main__':
-    main()
+def get_fg(argv, fg_def, bg):
+    if "-m" in argv:
+        i = inv_clr(bg)
+        return (i, i)
+    else:
+        if "-i" in argv:
+            return [inv_clr(i) for i in fg_def]
+        else:
+            return fg_def
+
+def get_bg(argv, bg_def):
+    if "-i" in argv:
+        return inv_clr(bg_def)
+    else:
+        return bg_def
+
+def inv_clr(h):
+    return 0xffffff - h
+
+import sys
+main(sys.argv)
