2a474a308ae331d25a297b1c97bb1c5c20715b70
kleinbottle4 <kleinbottle4@gmail.com>
2021-10-28 14:27:50 +0100

    foo

--- locus/main.py
+++ locus/main.py
@@ -1,58 +1,63 @@
 import pygame
 from pygame.locals import *
+from cmath import *
+from math import *
 import cmath
 import math
 
-WIDTH = 700
+WIDTH = 1000
 HWIDTH = int(WIDTH/2)
 GRID_SPACE = 10
-MAX = HWIDTH
-MIN = -HWIDTH
-SF = WIDTH/5
+SF = 100
+MAX = int(HWIDTH)
+MIN = -MAX
 
 BG = 0xffffff
 FG = 0x000000
-INK = 0xff0000
+INK = 0x0000ff
+
+E = 0.01
 
 def approx(x, y, e):
     return y - e <= x <= y + e
 
-FILTER = lambda z : approx(cmath.phase(z-SF) - cmath.phase(z), 1, 0.01)
+# FILTER = lambda z : \
+#         approx(phase(z-1)-phase(z+1), tau/12, E) \
+#         or approx(phase(z-1)-phase(z+1), -5 * tau/12, E)
+FILTER = lambda z : approx(abs(z-1), 1, 0.1)
 DISPLAY = pygame.display.set_mode((WIDTH, WIDTH))
 
 def main(argv):
-    pxarray = pygame.PixelArray(DISPLAY)
     DISPLAY.fill(BG)
-    pygame.display.update()
+    pxarray = pygame.PixelArray(DISPLAY)
     draw_grid_lines()
+    pygame.display.update()
     debug = "-d" in argv
     for x in range(MIN,MAX):
-        if debug:
-            print(x)
         for y in range(MIN,MAX):
-            z = complex(x,y)
+            z = complex(x/SF,y/SF)
             try:
                 if FILTER(z):
-                    pxarray[x + HWIDTH, HWIDTH - y] = INK
+                    px = int(x + HWIDTH)
+                    py = int(HWIDTH - y)
+                    pxarray[px, py] = INK
+                    if debug:
+                        print(z, end = ' : ')
+                        print(px, py)
             except Exception as e:
-                print(e)
+                print('!!!', e)
     pygame.display.update()
     del pxarray
-    while True:
-        if is_quit():
-            return 0
+    input()
 
 def draw_grid_lines():
     for i in range(0, WIDTH, GRID_SPACE):
         if i == HWIDTH:
-            thickness = 4
+            thickness = 2
         else:
             thickness = 1
         pygame.draw.line(DISPLAY, FG, (i, 0), (i, WIDTH), thickness)
         pygame.draw.line(DISPLAY, FG, (0, i), (WIDTH, i), thickness)
-
-def is_quit():
-    return pygame.locals.QUIT in [e.type for e in pygame.event.get()]
 
 import sys
 main(sys.argv)
